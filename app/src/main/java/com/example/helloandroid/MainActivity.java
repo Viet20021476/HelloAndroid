d93c37c4c22297a45f4c45894658e86a57d9a20b
Duy Viet <dviet307c@gmail.com>
2022-02-18 19:08:22 +0700

    first commit

---
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "You clicked!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
